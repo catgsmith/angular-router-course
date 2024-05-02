59c6b7427beecb339601e1cb81f08f8b0d14c3ae
Catherine Gaughan-Smith <catgsmith@users.noreply.github.com>
2024-05-02 08:49:42 +0100

    lazy loading courses

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,33 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule, PreloadAllModules, UrlSerializer} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule, PreloadAllModules, UrlSerializer } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
-  {
-    path: 'about',
-    component: AboutComponent
-  },
-  {
-    path: 'login',
-    component: LoginComponent
-  },
+    {
+        path: 'courses',
+        loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
 
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
-  exports: [RouterModule],
-  providers: [
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+    exports: [RouterModule],
+    providers: [
 
-  ]
+    ]
 })
 export class AppRoutingModule {
 
