5ef3e8dfcadea32f4ef377fe77e3ab0b2680abf5
Catherine Gaughan-Smith <catgsmith@users.noreply.github.com>
2024-09-22 21:40:45 +0100

    Replace Snapshot with Observable data Lesson Details

--- src/app/courses/lesson/lesson-detail.component.ts
+++ src/app/courses/lesson/lesson-detail.component.ts
@@ -11,16 +11,30 @@ import {map} from "rxjs/operators";
 })
 export class LessonDetailComponent implements OnInit {
 
-  lesson: LessonDetail;
+  lesson$: Observable<LessonDetail>;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private router: Router) {
 
     console.log("Created LessonDetailComponent...");
 
   }
 
   ngOnInit() {
-    this.lesson = this.route.snapshot.data["lesson"]; // this.route.data.pipe(map(data => data["lesson"]));
+    this.lesson$ = this.route.data.pipe(map(data => data["lesson"]));
   }
 
+    previous(lesson: LessonDetail) {
+
+      this.router.navigate(['lessons', lesson.seqNo - 1],
+          {relativeTo: this.route.parent});
+
+    }
+
+
+    next(lesson: LessonDetail) {
+
+        this.router.navigate(['lessons', lesson.seqNo + 1],
+            {relativeTo: this.route.parent});
+
+    }
 }
